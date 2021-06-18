6ac4150cf8ba58e8958c6c366cb10b42b29ad008
Leonard Colin <leooo.colin@gmail.com>
2021-06-18 18:15:10 +0200

    Refactor starting project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input className="new-todo" placeholder="What needs to be done?" autofocus />
+        </header>
+        <section className="main">
+        <input id="toggle-all" className="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+				<ul class="todo-list"></ul>
+        <footer class="footer">
+					<span class="todo-count"></span>
+					<ul class="filters">
+						<li>
+							<a href="#/" class="selected">All</a>
+						</li>
+						<li>
+						<a href="#/active">Active</a>
+						</li>
+						<li>
+						<a href="#/completed">Completed</a>
+						</li>
+					</ul>
+					<button class="clear-completed">Clear completed</button>
+				</footer>
+
+
+        </section>
+      </section>
+      
+			
     </div>
   );
 }
