cb4bc3839573e22a2ad50e2519a38c47d29e66d0
Leonard Colin <leooo.colin@gmail.com>
2021-06-18 19:32:25 +0200

    Create list elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+window.foobar = null;
+
+function Todo(props) {
+  window.foobar = props;
+  return (
+  <li>
+    <div className="view">
+      <input className="toggle" type="checkbox" />
+      <label>{props.item.text}</label>
+      <button className="destroy"></button>
+    </div>
+  </li>
+  );
+}
+
 function App() {
+  const items = [
+    {"text": "Rappel de Vaccin"},
+    {"text": "Faire courses"},
+    {"text": "Rappeler plombier"},
+  ];
+
   return (
     <div className="App">
       <section className="todoapp">
@@ -10,26 +31,31 @@ function App() {
           <input className="new-todo" placeholder="What needs to be done?" autofocus />
         </header>
         <section className="main">
-        <input id="toggle-all" className="toggle-all" type="checkbox" />
-        <label for="toggle-all">Mark all as complete</label>
-				<ul class="todo-list"></ul>
-        <footer class="footer">
-					<span class="todo-count"></span>
-					<ul class="filters">
-						<li>
-							<a href="#/" class="selected">All</a>
-						</li>
-						<li>
-						<a href="#/active">Active</a>
-						</li>
-						<li>
-						<a href="#/completed">Completed</a>
-						</li>
-					</ul>
-					<button class="clear-completed">Clear completed</button>
-				</footer>
-
-
+          <input id="toggle-all" className="toggle-all" type="checkbox" />
+          <label for="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            {items.map((item) => {
+              return (
+                <Todo item={item}/>
+              );
+            })}
+            
+          </ul>
+          <footer className="footer">
+            <span class="todo-count"></span>
+            <ul class="filters">
+              <li>
+                <a href="#/" class="selected">All</a>
+              </li>
+              <li>
+              <a href="#/active">Active</a>
+              </li>
+              <li>
+              <a href="#/completed">Completed</a>
+              </li>
+            </ul>
+            <button class="clear-completed">Clear completed</button>
+          </footer>
         </section>
       </section>
       
